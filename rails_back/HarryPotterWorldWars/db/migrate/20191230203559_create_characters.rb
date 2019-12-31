398732a71f32b851f6cebcd95c4ee2329422a1d5
github username <github email address>
2019-12-30 21:48:17 -0700

    fixed seed error, added join table instances, changed api for characters, added houses and spells to the character controller

--- rails_back/HarryPotterWorldWars/db/migrate/20191230203559_create_characters.rb
+++ rails_back/HarryPotterWorldWars/db/migrate/20191230203559_create_characters.rb
@@ -2,8 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :ancestory
-      t.string :image
+      t.string :ancestry
       t.references :house, null: false, foreign_key: true
       t.timestamps
     end
