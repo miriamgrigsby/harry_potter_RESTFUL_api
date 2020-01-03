81ba359c1cd59482014ab863142df98206245cde
github username <github email address>
2020-01-03 13:32:13 -0700

    added css for show page, fixed randomizer on show page, added embedded player

--- vanilla_front/show.js
+++ vanilla_front/show.js
@@ -1,6 +1,13 @@
-let winnerAnnouncement = document.createElement('h4')
+const headerContainer = document.getElementById('header-container')
+const houseGreeting = document.getElementById('house-greeting')
+const houseAdditionContainer = document.getElementById('house-addition')
+const spellsIntroContainer = document.getElementById('spells-intro')
+const spellsContainer = document.getElementById('spells-container')
+const imageContainer = document.getElementById('imgs')
+
+let winnerAnnouncement = document.createElement('h2')
 winnerAnnouncement.innerText = "And the winner is..."
-document.body.appendChild(winnerAnnouncement)
+headerContainer.appendChild(winnerAnnouncement)
 
 const searchParams = new URLSearchParams(window.location.search)
 const id = searchParams.get('id')
@@ -10,21 +17,29 @@ fetch (`http://localhost:3000/houses/${id}`)
     .then(houseWinner)
 
 function houseWinner(house){
-
-
+    const newestCharacterIndex = house["characters"].length-1
+    const newestCharacter = house["characters"][newestCharacterIndex]["name"]
         houseName = document.createElement('h1')
         houseAddition = document.createElement('h3')
         spellIntro = document.createElement('h4')
         houseSpells1 = document.createElement('h4')
         houseSpells2 = document.createElement('h4')
         houseSpells3 = document.createElement('h4')
+        customCharacter = document.createElement('h3')
+        empty = document.createElement('h1')
+
   
         houseName.innerText = `${house["name"]}!`
         houseAddition.innerHTML = `You earned a new house member: ${house["characters"][0]["name"]}`
         spellIntro.innerText = "You've earned these new spells:"
+        customCharacter.innerText = `Congrats ${newestCharacter}!`
 
-        
-        document.body.append(houseName, houseAddition, spellIntro)
+        houseGreeting.appendChild(houseName)
+        houseAdditionContainer.append(customCharacter, houseAddition)
+        spellsIntroContainer.appendChild(spellIntro)
+        spellsContainer.append(houseSpells1, houseSpells2, houseSpells3)
+        imageContainer.append(empty)
+
         // const characterSpell = house["spells"].map(spell=>{
         //     return spell
 
@@ -36,11 +51,10 @@ function houseWinner(house){
         const spell2 = house["characters"][1]["spells"][0]
         const spell3 = house["characters"][2]["spells"][0]
 
-        houseSpells1.innerHTML = `<a href="http://localhost:3001/showspells.html?id=${spell1.id}">${spell1["name"]}</a>`
-        houseSpells2.innerHTML = `<a href="http://localhost:3001/showspells.html?id=${spell2.id}">${spell2["name"]}</a>`
-        houseSpells3.innerHTML = `<a href="http://localhost:3001/showspells.html?id=${spell3.id}">${spell3["name"]}</a>`
+        houseSpells1.innerHTML = `<a style='color:blue' href="http://localhost:3001/showspells.html?id=${spell1.id}">${spell1["name"]}</a>`
+        houseSpells2.innerHTML = `<a style='color:blue' href="http://localhost:3001/showspells.html?id=${spell2.id}">${spell2["name"]}</a>`
+        houseSpells3.innerHTML = `<a style='color:blue' href="http://localhost:3001/showspells.html?id=${spell3.id}">${spell3["name"]}</a>`
 
-        document.body.append(houseSpells1, houseSpells2, houseSpells3)
             
 }
 
@@ -58,4 +72,23 @@ function getRandom(arr, n) {
     return result;
 }
 
+
+
+let playButton = document.createElement('button')
+playButton.innerText = "Play Me"
+imgs.appendChild(playButton)
+const video = document.getElementById("player")
+
+playButton.addEventListener('click', (event) => {
+    hiddenVideo()
+})
+
+function hiddenVideo() {
+    if (video.style.display === "block") {
+        video.style.display = "none"
+    } else {
+        video.style.display = "block"
+        // video.style = "text-align:center"
+    }
+}
 
